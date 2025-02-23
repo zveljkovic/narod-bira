776556bd8311e562873a1ae0445a140ac9a8c888
Zlatibor Veljkovic <zveljkovic@hotmail.com>
2025-02-23 22:48:14 +0100

    Small update to bg images and some scroll issues

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,26 @@
-var mainStartQuestionsButton = document.getElementById('start-questions');
-var mainSection = document.getElementById('main');
-var questionsSection = document.getElementById('questions');
+function createUUID() {
+    // http://www.ietf.org/rfc/rfc4122.txt
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+}
+
+setInterval(function() {
+    const classes = ['bg1', 'bg2', 'bg3', 'bg4'];
+    const body = document.body;
+    let currentClassIndex = classes.findIndex(c => body.classList.contains(c));
+    body.classList.remove(classes[currentClassIndex]);
+    let nextClassIndex = (currentClassIndex + 1) % classes.length;
+    body.classList.add(classes[nextClassIndex]);
+}, 10000)
 
 function getSessionId() {
     return localStorage.getItem('sessionId');
@@ -12,8 +32,8 @@ function setSessionId(sessionId) {
 
 document.addEventListener("DOMContentLoaded", function() {
     // Init random session id
-    if (getSessionId() === null) {
-        setSessionId(crypto.randomUUID());
+    if (!getSessionId()) {
+        setSessionId(createUUID());
     }
     document.getElementById('session-id').innerText = getSessionId();
 })
--- src/index.js
+++ src/index.js
@@ -203,6 +203,6 @@ app.get('/review', async (req, res) => {
 
 // Start the server
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '192.168.31.196', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
